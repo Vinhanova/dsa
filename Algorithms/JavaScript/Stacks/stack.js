5d7a99bc63f62934bc700ba2a2a59b295d8ad14f
Vinhanova <tiagovinhanova@gmail.com>
2023-01-27 10:46:10 +0000

    Create stack.js

---
+++ Algorithms/JavaScript/Stacks/stack.js
@@ -0,0 +1,63 @@
+class Stack {
+  constructor() {
+    this.items = []
+  }
+
+  push(item) {
+    this.items.push(item)
+  }
+
+  pop() {
+    if (!this.items.length) return null
+
+    return this.items.pop()
+  }
+
+  peek() {
+    if (!this.items.length) return null
+
+    return this.items.at(-1)
+  }
+
+  size() {
+    return this.items.length
+  }
+
+  isEmpty() {
+    return !this.size()
+  }
+
+  clear() {
+    this.items = []
+
+    return this.items
+  }
+
+  print() {
+    console.log(this.items)
+  }
+}
+
+let stack = new Stack()
+
+stack.push(5)
+stack.push(3)
+
+console.log(stack.peek()) // 3
+
+stack.pop()
+stack.push(9)
+
+console.log(stack.peek()) // 9
+
+stack.push(20)
+
+console.log(stack.size()) // 3
+
+stack.pop()
+
+stack.print() // [ 5, 9 ]
+
+stack.clear()
+
+console.log(stack.isEmpty()) // true
